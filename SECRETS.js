506d8cfb9d6583bb0018750eba968afd02d5f3fb
tipzutopia <tipzutopia@gmail.com>
2025-11-04 12:40:50 +0700

    Update SECRETS.js with real API keys
    Replaced placeholder secrets with actual API keys.

--- SECRETS.js
+++ SECRETS.js
@@ -1,11 +1,11 @@
 //? It is advisable to use environment variables instead of directly putting secrets in repository file but I have skipped this part as it would become complicated for many.
 //? Alternatively you can download the repository instead or forking and upload it from your account and keep it private, in that way, your secrets will not be exposed to the public.
 
-const APP_KEY = "YOUR_TWITTER_APP_KEY";
-const APP_SECRET = "YOUR_TWITTER_APP_SECRET";
-const ACCESS_TOKEN = "YOUR_TWITTER_ACCESS_TOKEN";
-const ACCESS_SECRET = "YOUR_TWITTER_ACCESS_SECRET";
-const GEMINI_API_KEY = "YOUR_GEMINI_API_KEY";
+const APP_KEY = "pWKTbvhjGElNWB4HhncpMmXtE";
+const APP_SECRET = "c0htKhgGkYWEb2FZNUnyMbfU19orkoep9xSyu7Z0cjoD265AAo";
+const ACCESS_TOKEN = "1985547438132989952-wVI7UnkzmrNcNLUJc9Grugi8tu5wer";
+const ACCESS_SECRET = "yuR1AHlOiD87dIokXmjgd8bhlAIeyDAeeXVbeK5ZQyd2K";
+const GEMINI_API_KEY = "AIzaSyB_MTtgcrl1DTIHcaUnk5u_phrZsD9_94I";
 
 const SECRETS = {
   APP_KEY,
