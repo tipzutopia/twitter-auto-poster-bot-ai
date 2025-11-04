f327607f3d7be7f5638fae303c67fe6adf2d130a
tipzutopia <tipzutopia@gmail.com>
2025-11-04 12:56:39 +0700

    Refine content generation prompt
    Updated the prompt for content generation to be more specific.

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ async function run() {
 
   // Write your prompt here
   const prompt =
-    "generate a web development content, tips and tricks or something new or some rant or some advice as a tweet, it should not be vague and should be unique; under 280 characters and should be plain text, you can use emojis";
+    "Create Viral Content";
 
   const result = await model.generateContent(prompt);
   const response = await result.response;
